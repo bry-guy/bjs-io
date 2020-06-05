a546b6693a8127fad1f8f914eb063859876b5b59
Bryan Smith <bryanj.smith91@gmail.com>
2020-06-04 22:57:35 -0700

    Actually use react semi-usefully.

--- app/javascript/components/blogs.tsx
+++ app/javascript/components/blogs.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import ReactHtmlParser from "react-html-parser";
 
-interface DisplayBlog {
+interface Blog {
 	title: string
 	date: Date
 	content: string
@@ -9,18 +9,33 @@ interface DisplayBlog {
 }
 
 export interface BlogsProps {
-	blogs: DisplayBlog[]
+	blogs: Blog[]
 }
 
 export class Blogs extends React.Component<BlogsProps, {}> {
   render() {
-	  const foo = this.props.blogs.map((item: DisplayBlog) => (
-		  <div>
-			  <div className="title" key={item.title}>{ReactHtmlParser(item.title)}</div>
-			  <div className="date" key={`${item.title}_${item.date}`}>{item.date}</div>
-			  <div className="preview" key={`${item.title}_${item.preview}`}>{item.preview}</div>
-		  </div>
-		  ));
-	  return(<div className="blogs">{foo}</div>);
+	  const blogs= this.props.blogs.map((item: Blog) => (
+		  <BlogItem blog={item}/>
+	  ));
+
+	  return(<div className="blogs">{blogs}</div>);
   }
 }
+
+interface BlogItemProps {
+	blog: Blog
+}
+
+class BlogItem extends React.Component<BlogItemProps, {}> {
+	render() {
+		const { blog } = this.props;
+		return(
+			<div>
+				<div className="title" key={blog.title}>{ReactHtmlParser(blog.title)}</div>
+				<div className="date" key={`${blog.title}_${blog.date}`}>{blog.date}</div>
+				<div className="preview" key={`${blog.title}_${blog.preview}`}>{blog.preview}</div>
+			</div>
+		);
+	}
+}
+
